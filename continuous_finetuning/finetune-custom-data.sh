868a06dbc95fc6336d45ad3a71cfbc00b822c278
azuna-remote-server <azuna-remote-server@example.com>
2025-06-23 15:48:41 +0800

    update

--- continuous_finetuning/finetune-custom-data.sh
+++ continuous_finetuning/finetune-custom-data.sh
@@ -2,12 +2,13 @@
 # ----------------- Scripts for origin Llama, PI, NTK and YaRN Methos-------------------
 RECIPE_NAME=custom_data
 METHOD_NAME=pi # option:[origin, pi, ntk, yarn]
-TRAINING_LENGTH=32768 
+TRAINING_LENGTH=16384 
+MODEL_PATH="../Llama-2-7b-hf/"
 WANDB_NAME=${RECIPE_NAME}_${METHOD_NAME}_${TRAINING_LENGTH}
 
 torchrun  --nproc_per_node=8 \
         fine-tune-custom-data.py  \
-        --model_name_or_path "meta-llama/Llama-2-7b-hf" \
+        --model_name_or_path ${MODEL_PATH} \
         --bf16 True \
         --output_dir ckpts/${RECIPE_NAME}/${WANDB_NAME} \
         --model_max_length ${TRAINING_LENGTH} \
@@ -15,20 +16,20 @@ torchrun  --nproc_per_node=8 \
         --low_rank_training False \
         --num_train_epochs 1 \
         --per_device_train_batch_size 1 \
-        --per_device_eval_batch_size 2 \
-        --gradient_accumulation_steps 32 \
-        --evaluation_strategy "no" \
+        --per_device_eval_batch_size 1 \
+        --gradient_accumulation_steps 1 \
+        --eval_strategy "no" \
         --save_strategy "epoch" \
         --save_total_limit 1 \
         --learning_rate 2e-5 \
         --weight_decay 0.0 \
         --warmup_steps 20 \
-        --deepspeed ds_configs/stage3_offload.json \
         --lr_scheduler_type "constant_with_warmup" \
         --logging_steps 1     \
         --tf32 True \
+        --bf16 True \
         --report_to "none" \
         --use_wandb False \
-        --dataset_dir 'data/*.jsonl' \
+        --dataset_dir '../data/*.jsonl' \
         --method_name ${METHOD_NAME} \
         --wandb_name ${WANDB_NAME} 
