4870b10fa068b16e85f0d8fa2210776d7383e869
Rui <32482525+monster119120@users.noreply.github.com>
2025-06-24 15:23:02 +0800

    update

--- continuous_finetuning/finetune-custom-data-long0.8-short0.2.sh
+++ continuous_finetuning/finetune-custom-data-long0.8-short0.2.sh
@@ -7,7 +7,7 @@ python data/sample_long.py \
     --save_dir "data_pool/sampled_data" \
     --cn 2.875 --baike 2.875 --en 2.875 --arxiv 2.875 --math 19 --code 8 --instruction 8.05 --log_278 1.725 --ai_search 1.725 \
     --ratio 0.8 \
-    --total_token_num 5e8   # 0.2B
+    --total_token_num 5e8   # 0.5B
 
 
 python data/sample_short.py \
@@ -15,7 +15,7 @@ python data/sample_short.py \
     --save_dir "data_pool/sampled_data" \
     --cn 2.875 --baike 2.875 --en 2.875 --arxiv 2.875 --math 19 --code 8 --instruction 8.05 --log_278 1.725 --ai_search 1.725 \
     --ratio 0.2 \
-    --total_token_num 5e8   # 0.2B
+    --total_token_num 5e8   # 0.5B
 
 
 
